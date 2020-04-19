9e066997271df3721ab1d571effe63d46ab82f48
Joshua Norman <jmnorma@clemson.edu>
2020-04-18 21:48:04 -0400

    Finished data collection

--- data_collection/report_data.py
+++ data_collection/report_data.py
@@ -1,10 +1,25 @@
 from top_extraction import top_extraction 
 
+import time 
+from datetime import datetime, date
+
 class report_data:
 
     def __init__(self):
         self.top = top_extraction()
         self.raw = self.top.get_top()
+        
+        #Chart Data
+        self.timeDic = {}
+
+        #Time Data
+        self.LastTime 
+        self.RunningTime = 0
+
+        #Date Data 
+        today = date.today()
+
+        self.date = today.strftime("%B %d, %Y")
         self.AAL = [] ## Acceptable Application List ( i.e. all )
         self._get_AAL()
 
@@ -13,6 +28,27 @@ class report_data:
         f = open("aal.txt","r")
         text = f.read()
         self.AAL = text.split("\n")
+
+    def getDate(self):
+        return self.date
+
+    def update(self):
+        #Retrive Top 
+        self.top.update()
+
+        #Record Time
+        now = datetime.now()
+        self.LastTime = now.strftime("%H:%M:%S")
+        self.RunningTime = self.RunningTime +1 
+
+        #Parce Data 
+        self.report_important()
+        self.update_timeDic
+
+        return [self.timeDic, self.LastTime, self.RunningTime ]
+
+    def get_report( self ):
+        return ( self.important_list, self.raw)
 
     def _report_running( self):
         self.running_list = []
@@ -46,6 +82,14 @@ class report_data:
         
         return self.important_list
 
+    def update_timeDic(self):
+        for key in self.important_list:
+            if key in self.timeDic:
+                print("Adding ONE")
+                self.timeDic[key]= self.timeDic[key]+ 1
+            else:
+                print("new ADDITION")
+                self.timeDic[key]= 1
 
 def main():
     report = report_data()
